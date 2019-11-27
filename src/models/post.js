229b7b701594073365661642aef171dc69ef5432
Mejrima <mejrima.smajilbasic@usi.ch>
2019-11-27 13:48:11 +0100

    added vimeo regex

--- src/models/post.js
+++ src/models/post.js
@@ -3,13 +3,15 @@
 const mongoose = require('mongoose');
 
 const postSchema = mongoose.Schema({
+  id: {type:Number},
   user: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
   title: { type: String, required: true },
   dateCreated: { type: Date, required: true, default: Date.now },
   comments: [{type: mongoose.Schema.Types.ObjectId, ref:'Comment', default: []}],
   upvotes: {type: Number, default: 1},
   downvotes: {type: Number, default: 0},
-  video: {type: mongoose.Schema.Types.ObjectId, ref:'Video'}
+  video: {type: mongoose.Schema.Types.ObjectId, ref:'Video'},
+  visibility: {type: String}
 });
 
 const Post = mongoose.model('Post', postSchema);
--- src/routes/post/router.js
+++ src/routes/post/router.js
@@ -1,24 +1,38 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+require('../models/post');
+require('../models/user');
 const Post = mongoose.model('Post');
 const User = mongoose.model('User');
 
-router.post('/clip/new', (req, res) => {
+router.post('/', (req, res) => {
 
+    if(!req.body || !req.body.title || !req.body.start || !req.body.end || !req.body.link)
     const user = new User ({
-        
-    })
+        username: req.body.username,
+    });
     const post = new Post ({
-        user: req.body.username,
+        id: req.body.id,
+        user: user,
         title: req.body.title,
-        video: {duration: req.body.end - req.body.start,
-        source: req.body.links}
+        video: {
+            duration: req.body.end - req.body.start,
+            source: req.body.links
+        },
+        visibility: req.body.visibility
     });
 
+    //add event listener to the upload button
     post.save()
     .then((saved) => {
         res.status(200);
         res.render('gallery.html');
+        res.end();
     })
-})
+    .catch((err) => {
+        res.status(400).end();
+    });
+});
+
+router.get('/')
