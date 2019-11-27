f06266a93598b921aaa7d35561838b22499531c2
Mejrima <mejrima.smajilbasic@usi.ch>
2019-11-27 16:02:28 +0100

    post done but not working completely

--- src/routes/post/router.js
+++ src/routes/post/router.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-require('../models/post');
-require('../models/user');
+require('../../models/post');
+require('../../models/user');
+require('../../models/video');
 const Post = mongoose.model('Post');
 const Video = mongoose.model('Video');
+
 const { check, validationResult } = require('express-validator');
 const youtubeRegex =
   new RegExp('^(?:(?:(?:https?:\\/\\/)?(?:www\\.)?youtube\\.com\\/watch\\?v=)|' +
@@ -13,24 +15,33 @@ const youtubeRegex =
 
 const timeRegex = /^(?:(?:(1?\d):)?([0-5]?\d):)?([0-5]\d)$/;
 
-router.post('/', [
-    check('link').matches(youtubeRegex),
-    check('start').matches(timeRegex),
-    check('end').matches(timeRegex),
-    check('title').not().isEmpty(),
-    check('title').isLength({max: 20})
-  ], (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(422).json({ errors: errors.array() });
-    }
+
+// router.post('/', (req, res) => {
+//     console.log("cioaiaoiaoa");
+// })
+
+router.post('/', (req, res) => {
+    // const errors = validationResult(req);
+    // if (!errors.isEmpty()) {
+    //     return res.status(422).json({ errors: errors.array() });
+    // }
 
     const video = new Video({
-        duration: req.body.end - req.body.start,
+        duration: Number(req.body.end) - Number(req.body.start),
             link: req.body.link,
             start: req.body.start,
             end: req.body.end
     });
+    console.log("duration: ", video.duration);
+    console.log("link: ", video.link);
+    console.log("start: ", video.start);
+    
+    //this is not saved for some reason
+    video.save()
+    .catch((err) => {
+        res.status(400).end();
+    });
+    
     const post = new Post ({
         user: req.user,
         title: req.body.title,
@@ -50,8 +61,10 @@ router.post('/', [
     });
 });
 
-router.get('/');
+// router.get('/');
 
 router.delete('/:postid', (req, res) => {
 
 });
+
+module.exports = router;
