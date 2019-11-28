13d1adfd0cf0e92c57c9680bef429f87f41026bd
Mejrima <mejrima.smajilbasic@usi.ch>
2019-11-28 19:33:29 +0100

    confused about the functions for likes

--- src/public/js/likes.js
+++ src/public/js/likes.js
@@ -1,5 +1,16 @@
+const upvotesBtn = document.getElementsByName("upvotes");
+
 function upvote() {
-    const upvotes = document.getElementsByName("upvotes").val();
+    
+    upvotesBtn.addEventListener('click', event => {
+        event.preventDefault();
+        doJSONRequest('POST', '/likes/', {}, ) //or PUT ?
+        .then(data => {
+            document.querySelector('upvotes').innerText = upvotesBtn.val() + 1;
+        })
+        .catch(console.error());
+    });
+    
 }
 function downvote() {
     const downvotes = document.getElementsByName("downvotes").val();
