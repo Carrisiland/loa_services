cbfe1c1a5b04c196873432b6b6ff488c221a7e79
Matteo Omenetti <omenem@usi.ch>
2019-11-30 17:44:31 +0100

    READ DESCRIPTION!!!!!
    Likes/Dilikes has been Implemented both for comments and posts.
    Since an array of users who have already liked the comment/post is missing from the model, every user can like/dislike every post/comment multiple times. I will fix this problem tomorrow.
    
    Since our website is not RealTime yet, when you push like/dislike the page renders nothing and you have to go back and reload the page. I am aware of that. However for now it's fine, this problem fill be fixed when we'll implement real time likes/dislikes.

--- src/routes/like/router.js
+++ src/routes/like/router.js
@@ -1,3 +1,4 @@
+// vim: set ts=2 sw=2 et tw=80:
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
@@ -10,10 +11,61 @@ const Video = mongoose.model('Video');
 const Comment = mongoose.model('Comment');
 const fetch = require('node-fetch');
 
-router.post('/' , (req, res) => {
-
-
+router.post('/comment/up/:id' , (req, res) => {
+  Comment.findById(req.params.id).then((comment) => {
+    comment.upvotes +=1;
+    return comment.save();
+  }).then((saved) => {
+    res.status(200);
+    res.end();
+  }).catch((err) => {
+    res.flash('error', err.toString());
+    res.status(500).render('gallery.html');
+  });
 });
+
+
+
+router.post('/comment/down/:id' , (req, res) => {
+  Comment.findById(req.params.id).then((comment) => {
+    comment.downvotes +=1;
+    return comment.save();
+  }).then((saved) => {
+    res.status(200);
+    res.end();
+  }).catch((err) => {
+    res.flash('error', err.toString());
+    res.status(500).render('gallery.html');
+  });
+});
+
+router.post('/post/up/:id' , (req, res) => {
+  Post.findById(req.params.id).then((post) => {
+    post.upvotes +=1;
+    return post.save();
+  }).then((saved) => {
+    res.status(200);
+    res.end();
+  }).catch((err) => {
+    res.flash('error', err.toString());
+    res.status(500).render('gallery.html');
+  });
+});
+
+router.post('/post/down/:id' , (req, res) => {
+  Post.findById(req.params.id).then((post) => {
+    post.downvotes +=1;
+    return post.save();
+  }).then((saved) => {
+    res.status(200);
+    res.end();
+  }).catch((err) => {
+    res.flash('error', err.toString());
+    res.status(500).render('gallery.html');
+  });
+});
+
+
 
 
 
