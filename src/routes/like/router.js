ab57b7e9fe93eb915b6157c256f20e4e9ca933da
Matteo Omenetti <omenem@usi.ch>
2019-11-30 17:01:39 +0100

    Added like router

--- src/routes/like/router.js
+++ src/routes/like/router.js
@@ -4,11 +4,17 @@ const mongoose = require('mongoose');
 require('../../models/post');
 require('../../models/user');
 require('../../models/video');
+require('../../models/comment');
 const Post = mongoose.model('Post');
 const Video = mongoose.model('Video');
+const Comment = mongoose.model('Comment');
 const fetch = require('node-fetch');
 
 router.post('/' , (req, res) => {
 
 
 });
+
+
+
+module.exports = router;
--- src/routes/post/router.js
+++ src/routes/post/router.js
@@ -1,5 +1,4 @@
 // vim: set ts=2 sw=2 et tw=80:
-
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
