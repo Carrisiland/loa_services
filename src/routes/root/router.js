35b142746859d6503d463a71db07c7614e9a73d4
Mejrima <mejrima.smajilbasic@usi.ch>
2019-12-01 14:44:00 +0100

    made the feed and gallery the same for now

--- src/routes/root/router.js
+++ src/routes/root/router.js
@@ -4,6 +4,14 @@
 
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+require('../../models/post');
+require('../../models/user');
+require('../../models/video');
+require('../../models/comment');
+const Post = mongoose.model('Post');
+const Video = mongoose.model('Video');
+const Comment = mongoose.model('Comment');
 
 router.get('/', (req, res) => {
   res.render('index.html');
@@ -11,9 +19,18 @@ router.get('/', (req, res) => {
 
 
 
+// router.get('/feed', (req, res) => {
+//   res.render('feed.html');
+// });
 router.get('/feed', (req, res) => {
-  res.render('feed.html');
+  Post.find({ visibility: 'public' }).populate('user').populate('video')
+    .then(posts => { console.log(posts); res.render('feed.html', { posts }) })
+    .catch(err => {
+      res.flash('error', err.toString());
+      res.status(500).render('feed.html');
+    });
 });
+
 
 /** router for /root */
 module.exports = router;
