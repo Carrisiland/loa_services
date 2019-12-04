8552520528c48acdb949ce71b379042ea41e8c78
Renato Iannace <iannar@usi.ch>
2019-12-04 18:16:07 +0100

    initial implementation search

---
+++ src/routes/search/router.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+require('../../models/post');
+require('../../models/user');
+require('../../models/video');
+require('../../models/comment');
+const User = mongoose.model("User");
+const Post = mongoose.model('Post');
+const Video = mongoose.model('Video');
+const Comment = mongoose.model('Comment');
+const { check, validationResult, sanitize } = require('express-validator');
+const fetch = require('node-fetch');
+
+
+router.get('/search', (req, res)=>{
+    let input = req.body.searchBar;
+    console.log("input:", input);
+
+})
