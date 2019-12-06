2c554744f84f3bdab928696090e3e115034568c8
Mejrima <mejrima.smajilbasic@usi.ch>
2019-12-06 13:47:44 +0100

    adding tag search

--- src/routes/search/router.js
+++ src/routes/search/router.js
@@ -13,21 +13,43 @@ const { check, validationResult, sanitize } = require('express-validator');
 const fetch = require('node-fetch');
 
 
-router.get('/', (req, res)=>{
+router.get('/', async (req, res) => {
     let input = req.query.searchBar;
     console.log("input:", input);
-    User.find({username: input}).then((found) => {
-        console.log(found)
-            if (found.length == 1) {
-                res.status(200);
-                res.render('searchResult.html' , {users: found});
-                console.log("CISOXS")
-                res.end();
-            }
-        }).catch((err) => {
-        res.status(500).redirect('/post/gallery');
-    });
+    let users = await User.find({username: input});
+    let posts = await Post.find({title : input});
+    if (users) {
+        res.status(200);
+        res.render('searchResult.html' , {found: users});
+        // console.log("CISOXS")
+        res.end();
+    } else if (posts) {
+        res.status(200);
+        res.render('searchResult.html' , {found: users});
+        // console.log("CISOXS")
+        res.end();
+    }
+    //how do we show both users and posts?? :(
+        
+    // User.find({username: input}).then((found) => {
+    //     console.log(found)
+    //         if (found.length == 1) {
+    //             res.status(200);
+    //             res.render('searchResult.html' , {users: found});
+    //             res.end();
+    //         }
+    //     }).catch((err) => {
+    //     res.status(500).redirect('/post/gallery');
+    // });
 })
-
+router.get('/:tag', async (req, res) => {
+    const tag = req.params.tag;
+    let posts = await Post.find({tags: tag});
+    if(posts) {
+        res.status(200);
+        res.render('searchResult.html' , {found: posts});
+        res.end();
+    }
+}); 
 
 module.exports = router;
