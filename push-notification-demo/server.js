608b8cc7683e442b9c392cb23cb6b713f9d5750d
Claudio Maggioni <claudio@maggioni.xyz>
2019-12-11 21:37:36 +0100

    Push notification demo added in repo

---
+++ push-notification-demo/server.js
@@ -0,0 +1,51 @@
+const webPush = require('web-push');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const VAPID_PUBLIC_KEY = 'BKaOrp9mnKVDGtPvsobCWJ2-KhNiZfZ9UdaedsjW9FzFgvl9Xpm21CY4ogbxS_-fU2-cqhiiMYt0ZTgjeO3W5I4';
+const VAPID_PRIVATE_KEY = 'YZraPi0vHmn9Rtp4YGFIdRbJLToWdKE-xe7MUPxswrA';
+
+// Set the keys used for encrypting the push messages.
+webPush.setVapidDetails(
+  'https://github.com/Carrisiland/vimtok',
+  VAPID_PUBLIC_KEY,
+  VAPID_PRIVATE_KEY
+);
+
+app.use(express.static('.'));
+app.use(bodyParser.json());
+
+app.get('/vapidPublicKey', function(req, res) {
+  res.send(VAPID_PUBLIC_KEY);
+});
+
+// Object to store to reach client (sort of a socket handle)
+let subscription = null;
+
+app.post('/register', function(req, res) {
+  subscription = req.body.subscription;
+  res.sendStatus(201);
+});
+
+app.post('/sendNotification', function(req, res) {
+  const payload = "test";
+  const options = {
+    TTL: 0
+  };
+
+  console.log(subscription);
+
+  // send to client previously stored in subscription
+  webPush.sendNotification(subscription, payload, options)
+    .then(() => {
+      res.sendStatus(201);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.sendStatus(500);
+    });
+});
+
+app.listen(3999);
+
