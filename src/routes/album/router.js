46f198a372f5b6a90a87a2b181fc998e4731f9c0
Renato Iannace <iannar@usi.ch>
2019-12-12 18:38:22 +0100

    update album router

--- src/routes/album/router.js
+++ src/routes/album/router.js
@@ -14,7 +14,23 @@ const Comment = mongoose.model('Comment');
 const { check, validationResult, sanitize } = require('express-validator');
 const fetch = require('node-fetch');
 
+router.post('/', async (req, res)=>{
+    let album = new Album({
+        user: req.user,
+        title: req.body.title,
+        visibility: req.body.visibility,
+        posts:[],
+        postNumbers:0
+    })
 
+    const saved = await album.save();
+
+    if (req.user) {
+        req.user.albums.push(saved);
+        await req.user.save();
+      }
+      res.redirect('/post/gallery');
+})
 
 
 
