324bff957a819fe104b21b04f646ccc5c910ecf8
Claudio Maggioni <claudio@maggioni.xyz>
2019-12-12 21:29:03 +0100

    Partially fixed formatting in follow/router.js

--- src/routes/follow/router.js
+++ src/routes/follow/router.js
@@ -1,3 +1,5 @@
+// vim: set ts=2 sw=2 et tw=80:
+
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
@@ -16,92 +18,94 @@ const fetch = require('node-fetch');
 router.get('/feed', (req, res) => {
   let user = req.user;
   User.findById(user.id).populate({
-    path : "following", 
+    path : "following",
     populate: [{
       path: "posts",
       populate: [{
         path: "video",
       },
         {path: "user"
-      }]
+        }]
     }]
   }).then(user =>{
     let postArr = [];
-    for (i in user.following){
-      for(j in user.following[i].posts){
+    for (i in user.following) {
+      for(j in user.following[i].posts) {
         postArr.push(user.following[i].posts[j])
       }
     }
-    postArr.sort(function(a,b){
+    postArr.sort((a, b) => {
       return Date.parse(b.dateCreated) - Date.parse(a.dateCreated);
     });
-    res.render('feed.html', {posts: postArr});
-    });
+    res.render('feed.html', { posts: postArr });
+  });
 });
 
 router.get('/feed/viewsort', (req, res) => {
   let user = req.user;
   User.findById(user.id).populate({
-    path : "following", 
+    path : "following",
     populate: [{
       path: "posts",
       populate: [{
         path: "video",
       },
-        {path: "user"
+      {
+        path: "user"
       }]
     }]
-  }).then(user =>{
+  }).then(user => {
     let postArr = [];
-    for (i in user.following){
-      for(j in user.following[i].posts){
+    for (i in user.following) {
+      for(j in user.following[i].posts) {
         postArr.push(user.following[i].posts[j])
       }
     }
-    postArr.sort(function(a,b){
-      return (b.views)- (a.views);
+    postArr.sort((a, b) => {
+      return b.views - a.views;
     });
-    res.render('feed.html', {posts: postArr});
-    });
+    res.render('feed.html', { posts: postArr });
+  });
 });
 
 router.get('/feed/dislikesort', (req, res) => {
   let user = req.user;
   User.findById(user.id).populate({
-    path : "following", 
+    path : "following",
     populate: [{
       path: "posts",
       populate: [{
         path: "video",
       },
-        {path: "user"
+      {
+        path: "user"
       }]
     }]
   }).then(user =>{
     let postArr = [];
-    for (i in user.following){
-      for(j in user.following[i].posts){
+    for (i in user.following) {
+      for(j in user.following[i].posts) {
         postArr.push(user.following[i].posts[j])
       }
     }
-    postArr.sort(function(a,b){
-      return (b.downvotes)- (a.downvotes);
+    postArr.sort((a, b) => {
+      return b.downvotes - a.downvotes;
     });
     res.render('feed.html', {posts: postArr});
-    });
+  });
 });
 
 router.get('/feed/likesort', (req, res) => {
   let user = req.user;
   User.findById(user.id).populate({
-    path : "following", 
+    path : "following",
     populate: [{
       path: "posts",
       populate: [{
         path: "video",
       },
         {path: "user"
-      }]
+        }]
     }]
   }).then(user =>{
     let postArr = [];
@@ -114,47 +118,47 @@ router.get('/feed/likesort', (req, res) => {
       return (b.upvotes)- (a.upvotes);
     });
     res.render('feed.html', {posts: postArr});
-    });
+  });
 });
 
 
 
 router.post("/:id",  (req, res)=>{
-  
-    User.findById(req.params.id).populate("followers").then(async (followedUser)=>{
-      if(req.user == undefined){
-        throw new Error;
+
+  User.findById(req.params.id).populate("followers").then(async (followedUser)=>{
+    if(req.user == undefined){
+      throw new Error;
+    }
+    const userId = req.user.id;
+    const user = await User.findById(req.user.id).populate("following");
+    let found = undefined;
+    for (i in followedUser.followers){
+      if (userId == followedUser.followers[i].id) {
+        found = followedUser.followers[i].id;
       }
-        const userId = req.user.id;
-        const user = await User.findById(req.user.id).populate("following");
-        let found = undefined;
-        for (i in followedUser.followers){
-            if (userId == followedUser.followers[i].id) {
-                found = followedUser.followers[i].id;
-              }
-        }
-        if (found == undefined){
-            followedUser.followers.push(req.user);
-            user.following.push(followedUser);
-        } else{
-            followedUser.followers = followedUser.followers.filter((follower) => {
-                return follower.id != userId;
-              });
-            user.following = user.following.filter((followingUser)=>{
-                return followingUser.id != followedUser.id;
-            });
-        }
-         
-         await user.save();
-         return followedUser.save(); ;
-    }).then((followed) => {
-        res.status(200).redirect('/profile/'+ followed.id);
-        res.end();
-      }).catch((err) => {
-        console.log('error = ',err)
-        //res.flash('error', err.toString());
-        res.status(500).render('gallery.html');
-    });
+    }
+    if (found == undefined){
+      followedUser.followers.push(req.user);
+      user.following.push(followedUser);
+    } else{
+      followedUser.followers = followedUser.followers.filter((follower) => {
+        return follower.id != userId;
+      });
+      user.following = user.following.filter((followingUser)=>{
+        return followingUser.id != followedUser.id;
+      });
+    }
+
+    await user.save();
+    return followedUser.save(); ;
+  }).then((followed) => {
+    res.status(200).redirect('/profile/'+ followed.id);
+    res.end();
+  }).catch((err) => {
+    console.log('error = ',err)
+    //res.flash('error', err.toString());
+    res.status(500).render('gallery.html');
+  });
 
 });
 
