ad29f3fd850a5aabeb3d1ff496cccf65d49b9cac
Renato Iannace <iannar@usi.ch>
2019-12-14 16:10:11 +0100

    added routers for album

--- src/routes/album/router.js
+++ src/routes/album/router.js
@@ -29,8 +29,38 @@ router.post('/', async (req, res)=>{
         req.user.albums.push(saved);
         await req.user.save();
       }
-      res.redirect('/post/gallery');
+      res.redirect('/profile/albums');
 })
+
+router.put('/', (req, res)=>{
+    Post.findById(req.params.id).then(async (found)=>{
+        if (!req.user){
+            throw new Error("You need to login to add the post to the album ");
+        }
+        const user = await User.findById(req.user.id).populate("albums");
+        let album = user.albums.filter( (album) =>{
+            return album.id == req.params.album;
+        })
+        album.posts.push(found);
+        album.postNumbers += 1;
+        await user.save();
+        res.status(200).render('/profile/albums');
+    })
+
+})
+
+router.get('/:id',  (req,res)=>{
+    Album.findById(req.params.id).populate("posts").then( found =>{
+    let postArr = [];
+    for (i in found.posts){
+        postArr.push(found.posts[i]);
+    }
+    postArr.sort((a, b) => {
+        return Date.parse(b.dateCreated) - Date.parse(a.dateCreated);
+    });
+    res.render('album.html', { posts: postArr, album: found.title });
+    });
+});
 
 
 
