573f05a8c261424900dc9ad76228df35f07533f4
Matteo Omenetti <omenem@usi.ch>
2019-12-15 17:41:27 +0100

    minor changes

--- src/public/js/album.js
+++ src/public/js/album.js
@@ -15,12 +15,15 @@ async function addAlbum(id) {
 
   let postId = id.substr(0, id.length - 5);
 
-  let res = await doFetchRequest('GET', '/album/user/', undefined, undefined);
+  let res = await doJSONRequest('GET', '/album/user/', undefined, undefined);
+
+  console.log(res);
 
   nunjucks.render('post/albumResults.html', res, (err, cont) => {
     document.getElementsByClassName("res")[0].innerHTML = cont;
 
     $(".albumNames").on('click', function(e) {
+      console.log("OOOOOjbnchjbdhjcde c");
       let albumId = jQuery(this).attr('id');
       doJSONRequest('PUT', '/album/', undefined, {id: postId, album: albumId})
     });
